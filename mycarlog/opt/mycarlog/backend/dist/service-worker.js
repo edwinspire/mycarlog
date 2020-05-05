6d3a6804a78239dc87ff5dcf5955ffdc7fc77a18
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2020-05-05 01:33:37 -0500

    Deploy

--- mycarlog/opt/mycarlog/backend/dist/service-worker.js
+++ mycarlog/opt/mycarlog/backend/dist/service-worker.js
@@ -16,7 +16,7 @@ workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 importScripts(
   "./externals/src/sw/service-worker-app.js",
-  "precache-manifest.8d385af6f783c20823ca64f6a87b1ec2.js"
+  "precache-manifest.8d411e95f6cecc2307708e41fe6394ac.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mycarlog-app"});
