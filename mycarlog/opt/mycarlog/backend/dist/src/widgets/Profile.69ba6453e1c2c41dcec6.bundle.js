8c26f267822656fd019c06fe0d4faa1e059fc711
Edwin De La Cruz <edwinspire@gmail.com>
2020-06-07 23:35:49 -0500

    Cambios para usar Store

---
+++ mycarlog/opt/mycarlog/backend/dist/src/widgets/Profile.69ba6453e1c2c41dcec6.bundle.js
@@ -0,0 +1,10 @@
+/*!
+ * 
+ * [Dojo](https://dojo.io/)
+ * Copyright [JS Foundation](https://js.foundation/) & contributors
+ * [New BSD license](https://github.com/dojo/meta/blob/master/LICENSE)
+ * All rights reserved
+ * 
+ */
+(window.dojoWebpackJsonpmycarlog3=window.dojoWebpackJsonpmycarlog3||[]).push([["src/widgets/Profile"],{296:function(o,e,r){o.exports={" _key":"mycarlog3/Profile",root:"Profile-m__root__e9fe79180X4"}},302:function(o,e,r){"use strict";r.r(e);var t=r(20),c=r(296);const s=Object(t.a)().properties();e.default=s(function({properties:o}){const{username:e}=o();return Object(t.k)("h1",{classes:[c.root]},[`Welcome ${e}!`])})}}]);
+//# sourceMappingURL=Profile.69ba6453e1c2c41dcec6.bundle.js.map
