1cfa31f8a615ac3f74012eea23269099661e1b4d
Edwin De La Cruz <edwinspire@gmail.com>
2020-06-08 23:05:36 -0500

    Cambios para usar Store

---
+++ mycarlog/opt/mycarlog/backend/dist/service-worker.js
@@ -0,0 +1,33 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
+
+importScripts(
+  "./externals/src/sw/service-worker-app.js",
+  "precache-manifest.85f45d6c0025f5bf7e37bd3600f0e9d4.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "mycarlog-app"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/.*\.(png|jpg|gif|svg)/, workbox.strategies.cacheFirst(), 'GET');
