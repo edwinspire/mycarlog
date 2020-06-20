4e02018f0bc3f234a45e1d749d68c275cf0ef515
Edwin De La Cruz <edwinspire@gmail.com>
2020-06-20 12:00:08 -0500

    Cambios para usar Store

---
+++ mycarlog/opt/mycarlog/backend/dist/precache-manifest.22815ef2822b5d59bb346c25ff1488c2.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "496e6b4603d321cead729ab1e2e09857",
+    "url": "1nCe3yIp.1nCe3yIp.ttf"
+  },
+  {
+    "url": "icon_36x36.ed1d92e026be1300d6ccc6ce46986423.png"
+  },
+  {
+    "revision": "7d4e39d9b1c6fc33fe50",
+    "url": "main.e0ce61601dfed7d0e99f.bundle.css"
+  },
+  {
+    "url": "main.7d4e39d9b1c6fc33fe50.bundle.js"
+  },
+  {
+    "url": "runtime/IntersectionObserver.db990ea6dca6c6fded3a.bundle.js"
+  },
+  {
+    "url": "runtime/ResizeObserver.c5d6e90fd25b56a7209b.bundle.js"
+  },
+  {
+    "url": "runtime/WebAnimations.cc56e212fc8faf8d1563.bundle.js"
+  },
+  {
+    "url": "runtime/blocks.6fa82dc5aa9eff6f90ed.bundle.js"
+  },
+  {
+    "url": "runtime/client.ee7aec5863fce7fb7ce9.bundle.js"
+  },
+  {
+    "url": "runtime/fetch.7e269737f625139accbf.bundle.js"
+  },
+  {
+    "url": "runtime/inert.5abed704caf265c00872.bundle.js"
+  },
+  {
+    "url": "runtime/pointerEvents.d181dae336daf4ef3e01.bundle.js"
+  },
+  {
+    "revision": "7107942252c4c4d782da",
+    "url": "src/widgets/About.ab1c0fa1741e47de74bf.bundle.css"
+  },
+  {
+    "url": "src/widgets/About.7107942252c4c4d782da.bundle.js"
+  },
+  {
+    "revision": "9cfb5d49cc9fd7571ff9",
+    "url": "src/widgets/AccountActivation/AccountActivation.0aefcefb5652691f88d1.bundle.css"
+  },
+  {
+    "url": "src/widgets/AccountActivation/AccountActivation.9cfb5d49cc9fd7571ff9.bundle.js"
+  },
+  {
+    "revision": "7928a0da36767e858f50",
+    "url": "src/widgets/Fueling/Fueling.85d6839cb5792d83cddf.bundle.css"
+  },
+  {
+    "url": "src/widgets/Fueling/Fueling.7928a0da36767e858f50.bundle.js"
+  },
+  {
+    "url": "src/widgets/Home.85bf01426e589687b5c9.bundle.js"
+  },
+  {
+    "revision": "73024dc2e2464812fc33",
+    "url": "src/widgets/Login/Login.90d476511f511e869f8a.bundle.css"
+  },
+  {
+    "url": "src/widgets/Login/Login.73024dc2e2464812fc33.bundle.js"
+  },
+  {
+    "revision": "d5daa9333140b923c8db",
+    "url": "src/widgets/Register/Register.02b0cb95757743b83cf0.bundle.css"
+  },
+  {
+    "url": "src/widgets/Register/Register.d5daa9333140b923c8db.bundle.js"
+  },
+  {
+    "revision": "3cb7a15870b929c82993",
+    "url": "src/widgets/Vehicle/Vehicle.a44d94b64e653e9c6465.bundle.css"
+  },
+  {
+    "url": "src/widgets/Vehicle/Vehicle.3cb7a15870b929c82993.bundle.js"
+  },
+  {
+    "revision": "361f9b9d2f110b29cff8",
+    "url": "src/widgets/VehicleCards/VehicleCards.d9552cf80eb3d9604dcd.bundle.css"
+  },
+  {
+    "url": "src/widgets/VehicleCards/VehicleCards.361f9b9d2f110b29cff8.bundle.js"
+  },
+  {
+    "url": "icon_48x48.f09d828f7dd9896e985e794524da9cb4.png"
+  },
+  {
+    "url": "icon_72x72.e90335de12117005452d928ed66d2f45.png"
+  },
+  {
+    "url": "icon_96x96.e42c16208a240c921e6f47acbcda02ad.png"
+  },
+  {
+    "revision": "0f16a5a8682fc9924e55e289f7d740c3",
+    "url": "index.html"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "fa-regular-400.2XyVpsID.svg"
+  },
+  {
+    "revision": "91610db3b60d79b76b948d358177b106",
+    "url": "dojo.6Inu1doS.ttf"
+  },
+  {
+    "revision": "06ad567f4da20aa1c32232e76e640921",
+    "url": "dojo.x2mhKVP8.svg"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "fa-brands-400.3gOtyJ7o.eot"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "fa-regular-400.2czULVpn.eot"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "fa-solid-900.F7N4RM7h.eot"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "fa-brands-400.2aomTlKp.woff2"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "fa-brands-400.3cMvshZv.woff"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "fa-brands-400.2GCLS2M4.ttf"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "fa-brands-400.2G---LNE.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "fa-regular-400.-UWN4g_D.woff2"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "fa-regular-400.3hlH74EH.woff"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "fa-regular-400.1UYPHSx6.ttf"
+  },
+  {
+    "revision": "682768547d6d5d264b5d74b15b5fd62c",
+    "url": "dojo.IRBZrInh.woff"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "fa-solid-900.30zBJ5OP.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "fa-solid-900.xk82AmfM.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "fa-solid-900.3MHyxCQm.ttf"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "fa-solid-900.ZtWMM2jN.svg"
+  },
+  {
+    "revision": "0deb6ad52f78113aa47c8a4fbb042f4b",
+    "url": "favicon.1bbMiXjU.ico"
+  },
+  {
+    "revision": "9d65ebbfd3f8e860aff82b7e8e7425d2",
+    "url": "externals/./src/sw/service-worker-app.js"
+  },
+  {
+    "url": "icon_192x192.5c4788e2149fc2ae44d9e63707a81bc8.png"
+  },
+  {
+    "url": "icon_144x144.ad137bff6e774fa1c00d0f9d5520cde6.png"
+  },
+  {
+    "url": "bootstrap.87cae3644772f093a578.bundle.js"
+  },
+  {
+    "revision": "87cae3644772f093a578",
+    "url": "bootstrap.82c71bd9f1a84dee17ca.bundle.css"
+  },
+  {
+    "revision": "1c0d71dafe5be2304fa510afc28d9524",
+    "url": "ABoT2HN2.ABoT2HN2.woff"
+  },
+  {
+    "revision": "e441343259a01a08dd9ec20a1990c87f",
+    "url": "1_O90p2I.1_O90p2I.svg"
+  }
+];
